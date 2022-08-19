b0d5ccf8faea3d6acfb0927c9f0067b7f75a7520
orgtre <orgtre@posteo.net>
2022-08-19 14:42:01 +0200

    Refactor `ankiorg-sql.el` and make improvements along the way

--- ankiorg-sql.el
+++ ankiorg-sql.el
@@ -16,12 +16,6 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-;;; Commentary:
-
-;; #TODO Why does sqlite-open-readonly sometimes not work?
-;;       Only works with sqlite-open-readwrite.
-;;       Try again with sqlite-open-readonly.
-
 
 ;;; Code:
 
@@ -70,165 +64,170 @@ of a difference anymore."
 ;; TODO and ankiorg-sql-pick-tag?
 
 
-;; * ankiorg-sql-exec-get-alists: general query utility function
+;; * SQL interface functions
 
-(defun ankiorg-sql-exec-get-alists (query)
-  "Execute sqlite3 QUERY (a string) and return results as list of alists.
-Wrapper around `sqlite3-exec'. Runs against the database given in
-`ankiorg-sql-database'."
+(defun ankiorg-sql-exec (query callback)
+  "Run SQLite QUERY (a string) on the Anki database.
+Function CALLBACK is called for each row returned by QUERY. Wrapper
+around `sqlite3-exec' which opens and closes `ankiorg-sql-database'."
+  ;; TODO Why does it randomly work or not with sqlite-open-readonly?
   (let ((db (sqlite3-open ankiorg-sql-database sqlite-open-readwrite))
-	(res))
-    (sqlite3-exec db query
-                  (lambda (ncols row names)
-                    (let ((i 0)
-                          (li nil))
-                      (while (< i ncols)
-                        (push (cons (intern (nth i names)) (nth i row)) li)
-                        (setq i (1+ i)))
-                      (setq li (reverse li))
-                      (push li res))))
-    (sqlite3-close db)
-    res))
+	(result nil))
+    (sqlite3-exec db query callback)
+    (sqlite3-close db)))
 
 
-;; * ankiorg-sql-note-ids
+(defun ankiorg-sql-pick-deck ()
+  "Select a deck from the list of decks used in `ankiorg-sql-database'."
+  (let* (decks
+	 (query "select name from decks")
+	 (callback (lambda (_ncols row _colnames)
+		     (let ((deck (car row)))
+		       (push (replace-regexp-in-string "\^_" "::" deck)
+			     decks)))))
+    (ankiorg-sql-exec query callback)
+    (completing-read "Deck: "
+                     (sort decks #'string-lessp))))
+
 
 (defun ankiorg-sql-note-ids (&optional deck)
   "Return list of all note-id's in Anki.
 If DECK is non-nil, return only note-id's in DECK."
-  (let ((db (sqlite3-open ankiorg-sql-database sqlite-open-readwrite))
-	;; the 'or' is used to also match subdecks
-	(query (if deck
-		   (let ((deck (replace-regexp-in-string "::" "" deck)))
-		     (format
-		      "pragma case_sensitive_like=on;
+  (let* (note-ids
+	 (query (if deck
+		    (let ((deck (replace-regexp-in-string "::" "" deck)))
+		      (format
+		       ;; the 'or' is used to also match subdecks
+		       "pragma case_sensitive_like=on;
 		      select distinct nid from cards where did in
                       (select id from decks where name like
                       '%s' or name like '%s%%')" deck deck))
-		 "select distinct nid from cards"))
-	(ids-in-anki))
-    (sqlite3-exec db query
-                  (lambda (ncols row names)
-                    (push (string-to-number (car row)) ids-in-anki)))
-    (sqlite3-close db)
-    ids-in-anki))
+		  "select distinct nid from cards"))
+	 (callback (lambda (_ncols row _colnames)
+                     (push (string-to-number (car row)) note-ids))))
+    (ankiorg-sql-exec query callback)
+    note-ids))
 
 
-;; * ankiorg-sql-get-notes
+(defun ankiorg-sql-exec-get-alists (query)
+  "Run SQLite QUERY (a string) and return results as list of alists.
+Wrapper around `ankiorg-sql-exec' which in turn wraps `sqlite3-exec'."
+  (let* (result
+	 (callback (lambda (ncols row names)
+                     (let ((i 0)
+                           (li nil))
+                       (while (< i ncols)
+                         (push (cons (intern (nth i names)) (nth i row)) li)
+                         (setq i (1+ i)))
+                       (setq li (reverse li))
+                       (push li result)))))
+    (ankiorg-sql-exec query callback)
+    result))
+
 
 (defun ankiorg-sql-get-notes (note-ids &optional _deck)
-  "Queries sqlite3 for note data given a list of NOTE-IDS.
-Makes sure that field names are added properly. Returns a list of notes in
-alist format."
-
-  ;; #TODO this runs two sql queries and one loop across fields per note-id --
-  ;; might be slow, but how to handle the field names, which are stored on
-  ;; multiple rows and whose order matters, otherwise? In MySQL one can specify
-  ;; ORDER BY within group_concat, but SQLite doesn't support this
-
-  ;; - instead the order of elements returned by group_concat is arbitrary...
+  "Return list of notes in alist format with note-ids in NOTE-IDS.
+Makes sure that field names are added properly."
+  ;; #TODO better progress indication
+  ;; #TODO this runs two sql queries and one loop across fields per note-id,
+  ;; hence this might be slow, but how to handle the field names,
+  ;; which are stored on multiple rows and whose order matters, otherwise?
+  ;; In MySQL one can specify ORDER BY within group_concat,
+  ;; but SQLite doesn't support this - instead the order of elements
+  ;; returned by group_concat is arbitrary...
   ;; The ord column could be used for this: GROUP_CONCAT(ord || ':' || name)
   ;; but this doesn't seem to become faster and less complex than currently.
-  
-  (setq
-   final
-   (let ((value)(res)(mres)(out))    
-     (dolist (note-id note-ids value)
-       (setq res
-	     (ankiorg-sql-exec-get-alists
-	      (format
-	       (concat "SELECT notes.id AS 'note-id', mid, notetypes.name AS "
-		       "'note-type', a.deck, trim(tags) AS tags, flds AS fields"
-		       " FROM notes INNER JOIN (SELECT cards.nid, "
-		       "group_concat(decks.name, '\x1f') AS deck FROM cards "
-		       "INNER JOIN decks ON cards.did = decks.id "
-		       "WHERE cards.nid = %s) AS a ON notes.id = a.nid "
-		       "INNER JOIN notetypes ON notes.mid = notetypes.id")
-	       note-id)))
-       ;; could select mod here too
-       ;; old query without decks and renaming:
-       ;; (concat "select notes.id, mid, notetypes.name, trim(tags), flds from "
-       ;; 	       "notes left join notetypes on notes.mid = notetypes.id "
-       ;; 	       "where notes.id = %s"
-       
-       (setq mres
-	     (reverse
-	      (ankiorg-sql-exec-get-alists
-	       (format "select name from fields where ntid = %s order by ord"
-		       (cdr (assoc 'mid (car res)))))))
-       ;; display warning when note has cards in multiple decks
-       ;; #TODO this shouldn't display for nested decks
-       (when (> (length (split-string (cdr (assoc 'deck (car res))) "\^_")) 1)
-	 (display-warning
-	  'ankiorg
-	  (format
-	   (concat "Note with note-id %s has cards in multiple decks. "
-		   "Anki-editor does not handle this properly, hence the deck "
-		   "property of this org note will be unreliable.")
-	   (cdr (assoc 'note-id (car res))))))
-       
-       ;; given the above lists this will output the fields in the right format
-       (setq out
-	     (let ((i 0)
-                   (flds-list (split-string
-			       (cdr (assq 'fields (car res)))
-			       "\^_"))
-                   (li nil)
-                   (elem))
-	       
-               (while (< i (length mres))
-		 (setq elem (cons (cdr (car (nth i  mres))) (nth i flds-list)))
-		 (push elem li)
-		 (setq i (1+ i)))
-	       
-               (setq li (reverse li))))
-
-       (setcdr (assoc 'fields (car res)) out)
-       (setcdr (assoc 'note-id (car res))
-	       (string-to-number (cdr (assoc 'note-id (car res)))))
-       (assq-delete-all 'mid (car res))
-       (push (car res) value)
-       ))))
+   (let (note notes)
+     (dolist (note-id note-ids notes)
+       (setq note (ankiorg-sql-note-without-field-alist note-id))
+       (setq note (ankiorg-sql-note-add-field-alist note))
+       (assq-delete-all 'mid note)
+       (push note notes))))
 
 
-;; * ankiorg-sql-pick-deck
+(defun ankiorg-sql-note-without-field-alist (note-id)
+  "Get note NOTE-ID in alist form, but without field alist.
+Instead only a concatenated string of field contents is returned.
+In this string fields are separated by ''."
+  ;; TODO check the SQL, can it be simplified?
+  ;; TODO can cards really be in multiple non-nested decks?
+  ;;      Or why group_concat(decks.name, '\x1e')?
+  ;; TODO does the deck get set correctly in all cases?
+  (let ((note
+	 (car (ankiorg-sql-exec-get-alists
+	       (format "SELECT notes.id AS 'note-id', mid, notetypes.name AS
+	               'note-type', a.deck, trim(tags) AS tags, flds AS fields
+	               FROM notes INNER JOIN (SELECT cards.nid, 
+	               group_concat(decks.name, '\x1e') AS deck FROM cards 
+	               INNER JOIN decks ON cards.did = decks.id 
+	               WHERE cards.nid = %s) AS a ON notes.id = a.nid 
+	               INNER JOIN notetypes ON notes.mid = notetypes.id"
+		       note-id)))))
+    (setcdr (assoc 'note-id note)
+	    (string-to-number (alist-get 'note-id note)))
+    (ankiorg-sql-warn-if-multiple-decks note)
+    (setcdr (assoc 'deck note)
+	    (replace-regexp-in-string "\^_" "::" (alist-get 'deck note)))
+    note))
 
-(defun ankiorg-sql-pick-deck ()
-  "Select a deck from the list of decks used in `ankiorg-sql-database'."
-  (interactive)
-  (setq res nil)
-  (setq db (sqlite3-open ankiorg-sql-database sqlite-open-readwrite))
-  (sqlite3-exec db "select id, name from decks"
-		(lambda (ncols row names)
-		  (let ((id (car row))
-			(flds (cdr row)))
-		    (push (replace-regexp-in-string
-			   "\^_" "::" (car flds))
-			  res))))
-  (sqlite3-close db)
-  (completing-read "Deck: "
-                   (sort res #'string-lessp)))
+
+(defun ankiorg-sql-warn-if-multiple-decks (note)
+  "Show warning if NOTE has cards in multiple decks."
+  (when (> (length (split-string (cdr (assoc 'deck note)) "\x1e")) 1)
+    (display-warning
+     'ankiorg
+     (format
+      (concat "Note with note-id %s has cards in multiple decks. "
+	      "Anki-editor does not handle this properly, hence the deck "
+	      "property of this org note will be unreliable.")
+      (cdr (assoc 'note-id note))))))
 
 
-;; * ankiorg-sql-pick-tag
+(defun ankiorg-sql-note-add-field-alist (note)
+  "Add field alist to alist NOTE."
+  (let* ((field-names
+	  (ankiorg-sql-field-names (alist-get 'mid note)))
+	 (field-contents
+	  (split-string (alist-get 'fields note) "\^_"))
+	 (field-alist
+	  (cl-mapcar 'cons field-names field-contents)))
+    (setcdr (assoc 'fields note)
+	    field-alist)
+    note))
+
+
+(defun ankiorg-sql-field-names (model-id)
+  "Get field names belonging to note type (note model) with MODEL-ID."
+  (let* (fields
+	 (query (format "select name from fields where ntid = %s
+                        order by ord desc"
+			model-id))
+	 (callback (lambda (_ncols row _colnames)
+		     (push (car row) fields))))
+    (ankiorg-sql-exec query callback)
+    fields))
+
 
 (defun ankiorg-sql-pick-tag ()
   "Select a tag from the list of tags used in `ankiorg-sql-database'."
   (interactive)
-  (setq res nil)
-  (setq db (sqlite3-open ankiorg-sql-database sqlite-open-readwrite))
-  ;; sql query taken from here: https://stackoverflow.com/a/32051164
-  (sqlite3-exec db "WITH split(word, str) AS (
-    SELECT '', trim(tags)||' ' FROM notes
-    -- SELECT '', '  A B CC DDD 101 fff '||' '
-    UNION ALL SELECT
-    substr(str, 0, instr(str, ' ')),
-    substr(str, instr(str, ' ')+1)
-    FROM split WHERE str!='') SELECT DISTINCT word FROM split WHERE word!=''
-    ORDER BY word COLLATE NOCASE DESC"
-		(lambda (ncols row names) (push row res)))
-  (sqlite3-close db)
-  (completing-read "Choose a deck: " res))
+  (let* (result
+	 (query
+	  ;; sql query taken from here:
+	  ;; https://stackoverflow.com/a/32051164
+	  "WITH split(word, str) AS
+          (SELECT '', trim(tags)||' ' FROM notes
+          -- SELECT '', '  A B CC DDD 101 fff '||' '
+          UNION ALL SELECT
+          substr(str, 0, instr(str, ' ')),
+          substr(str, instr(str, ' ')+1)
+          FROM split WHERE str!='')
+          SELECT DISTINCT word
+          FROM split WHERE word!=''
+          ORDER BY word COLLATE NOCASE DESC")
+	 (callback (lambda (_ncols row _colnames) (push row result))))
+    (ankiorg-sql-exec query callback)
+    (completing-read "Choose a deck: " result)))
 
 
 (provide 'ankiorg-sql)
